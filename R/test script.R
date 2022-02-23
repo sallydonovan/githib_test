a569b90e783235c93cbcbc1814e45af4fec2a4e4
Sally Donovan <sallydonovan@infra04-wg324.eduroam.wireless.umn.edu>
2022-02-23 12:59:27 -0600

    tesst2

--- R/test script.R
+++ R/test script.R
@@ -1,1 +1,16 @@
 # test test test
+
+# If you can't push your project to githib using a password, use a token
+# to create a token:
+# install "use this" tool to create token
+#install.packages("usethis")
+library(usethis)
+# this will take you to the githbub page to create a token
+
+# set up token for access to push/pull 
+#install.packages("gitcreds")
+library(gitcreds)
+gitcreds_set()
+
+# change to push
+
